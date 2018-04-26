65a3d62f9239700dc794e107c12695685ae9dc2d
Tiffani Rogers <TRoger56@ford.com>
2018-04-26 16:33:56 -0400

    Removed Gradle Properties Proxy

--- app/src/main/java/mmjp/fsm/ford/com/mmjp/app/route/providers/lyft/rider/utils/LyftConstants.java
+++
@@ -1,9 +0,0 @@
-package mmjp.fsm.ford.com.mmjp.app.route.providers.lyft.rider.utils;
-
-public class LyftConstants {
-    final public static String BASE_URL = "https://api.lyft.com";
-    final public static String REDIRECT_URI = "mmjp.fsm.ford.com://redirect";
-    final public static String CLIENT_ID = "MnJB-RVRtHW4";
-    final public static String CIENT_SECRET = "thqfwfugk4Wgz6H3YzezBCip4p3QOTMU";
-    final public static String CLIENT_TOKEN = "TM54iBmicequ7e06Vx6HTh6IdP0nW9wWNOHZSHY3RUopXWLxyEfdB3nr5HFzl0eBHvdOFUlWViMKLhUfNWwkn2+Olc+GhJjt4roMWs3i9J+VDU5GWvO/lwg=";
-}
--- app/src/main/java/mmjp/fsm/ford/com/mmjp/app/route/providers/uber/models/RiderSessionModel.java
+++
@@ -1,6 +0,0 @@
-package mmjp.fsm.ford.com.mmjp.app.route.providers.uber.models;
-
-public class RiderSessionModel {
-
-
-}
